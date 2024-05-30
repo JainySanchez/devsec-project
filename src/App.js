3369abefbbbd1f7d024ee9214e7b7da274d4b0d6
JainySanchez <jainysancheza@gmail.com>
2024-05-30 13:47:53 -0500

    Add Configuraions uptade

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'; // Importa Route y Routes de react-router-dom
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'; 
 import Login from './Login';
 import Welcome from './Welcome';
 import Register from './Register';
-import ChangePassword from './ChangePassword'; // Importa el componente de cambio de contraseña
+import ChangePassword from './ChangePassword'; 
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,12 +18,10 @@ function App() {
   };
 
   const handleRegister = (email, password) => {
-    // Aquí puedes añadir la lógica para registrar al usuario
     console.log('Registrando usuario con email:', email, 'y password:', password);
   };
 
   const handleChangePassword = (oldPassword, newPassword) => {
-    // Aquí puedes añadir la lógica para cambiar la contraseña
     console.log('Cambiando contraseña desde', oldPassword, 'a', newPassword);
   };
 
